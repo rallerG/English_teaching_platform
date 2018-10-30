bf92263bc5f59e631f16e548c81438395bc02833
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-10-30 09:56:21 +0100

    Added correct titles and icons
    First commit

--- app/src/main/java/com/gruppe/englishteachingplatfrom/MainActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_matches) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_pending) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
