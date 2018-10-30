7584afca405d9dc337360da957cd94a705351278
Cilian <32563603+Cilian@users.noreply.github.com>
2018-10-30 10:50:55 +0100

    popup function

--- app/src/main/java/com/gruppe/englishteachingplatfrom/VoicePopUpActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/VoicePopUpActivity.java
@@ -1,15 +1,18 @@
 package com.gruppe.englishteachingplatfrom;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class VoicePopUpActivity extends Fragment {
+
+    Button upload;
+    TextView notNow;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,27 @@ public class VoicePopUpActivity extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voice_pop_up, container, false);
+        View rootView=inflater.inflate(R.layout.fragment_voice_pop_up,container,false);
+
+        upload = rootView.findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+               //Go to upload activity
+            }
+        });
+
+        notNow = rootView.findViewById(R.id.notNow);
+        notNow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                getActivity().onBackPressed();
+            }
+        });
+
+        return rootView;
     }
 
 }
