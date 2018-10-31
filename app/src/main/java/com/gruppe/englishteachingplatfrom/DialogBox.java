e8e67d6c390b5983852606b44266754d8f4f8d0d
Patrick Marcell Madsen <s175209@student.dtu.dk>
2018-10-31 17:07:33 +0100

    implemented the java code for DialogBox

--- app/src/main/java/com/gruppe/englishteachingplatfrom/DialogBox.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/DialogBox.java
@@ -1,6 +1,5 @@
 package com.gruppe.englishteachingplatfrom;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,26 +7,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
-public class DialogBox extends Fragment {
 
+public class DialogBox extends Fragment implements View.OnClickListener {
 
-
-    public static DialogBox newInstance() {
-        return new DialogBox();
-    }
+    Button cancelButton, sendButton;
+    ImageView teacherImage;
+    TextView teacherInfo, confirmationText;
+    RatingBar ratingBar;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dialog_box_fragment, container, false);
+        View vw = inflater.inflate(R.layout.dialog_box_fragment, container, false);
+
+        cancelButton = vw.findViewById(R.id.OkayButton);
+        sendButton = vw.findViewById(R.id.SendButton);
+        teacherImage = vw.findViewById(R.id.TeacherImage2);
+        teacherInfo = vw.findViewById(R.id.TeacherInfo2);
+        confirmationText = vw.findViewById(R.id.ConfirmationMessage);
+        ratingBar = vw.findViewById(R.id.RatingBar);
+
+        cancelButton.setOnClickListener(this);
+        sendButton.setOnClickListener(this);
+
+
+        return vw;
     }
+
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onClick(View v) {
 
-        
+        if(v == sendButton){
+            getFragmentManager().beginTransaction().replace(R.id.fragment, new ConfirmationBox()).addToBackStack(null).commit();
+        }
+        else{
+
+        }
+
+
     }
-
 }
