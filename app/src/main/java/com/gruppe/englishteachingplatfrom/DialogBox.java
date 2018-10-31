fa2df6945b02fa5936d856a72edfd5fe5dc77836
Patrick Marcell Madsen <s175209@student.dtu.dk>
2018-10-31 13:49:24 +0100

    Created the intial fragment and started on the layout

---
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/DialogBox.java
@@ -0,0 +1,33 @@
+package com.gruppe.englishteachingplatfrom;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogBox extends Fragment {
+
+    private DialogBoxViewModel mViewModel;
+
+    public static DialogBox newInstance() {
+        return new DialogBox();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_box_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(DialogBoxViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/DialogBoxViewModel.java
@@ -0,0 +1,7 @@
+package com.gruppe.englishteachingplatfrom;
+
+import android.arch.lifecycle.ViewModel;
+
+public class DialogBoxViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
