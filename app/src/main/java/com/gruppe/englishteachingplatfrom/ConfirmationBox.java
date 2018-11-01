cdc4d67a910e973888d205c88fb27bb56540786e
Patrick Marcell Madsen <s175209@student.dtu.dk>
2018-11-01 14:46:28 +0100

    added an empty contructor and attributes

--- app/src/main/java/com/gruppe/englishteachingplatfrom/ConfirmationBox.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/ConfirmationBox.java
@@ -1,23 +1,37 @@
 package com.gruppe.englishteachingplatfrom;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 
 public class ConfirmationBox extends Fragment implements View.OnClickListener {
 
     Button okayButton;
+    ImageView teacherImage;
+    TextView teacherInfo, confirmationText;
+    RatingBar ratingBar;
+
+    public ConfirmationBox(){
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View vw = inflater.inflate(R.layout.fragment_confirmation_box, container, false);
+
+        teacherImage= vw.findViewById(R.id.TeacherImage2);
+        teacherInfo = vw.findViewById(R.id.TeacherInfo2);
+        confirmationText = vw.findViewById(R.id.ConfirmationMessage2);
+        ratingBar = vw.findViewById(R.id.RatingBar2);
+
         okayButton = vw.findViewById(R.id.OkayButton);
         okayButton.setOnClickListener(this);
 
