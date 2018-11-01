66ca1b6ae69dfd878ef349a8b51fd6441b9f0001
MagnusKochDTU <s175189@student.dtu.dk>
2018-11-01 14:12:55 +0100

    added signup button functions

--- app/src/main/java/com/gruppe/englishteachingplatfrom/signupActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/signupActivity.java
@@ -1,13 +1,15 @@
 package com.gruppe.englishteachingplatfrom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,20 @@ public class SignupActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Button signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.signup :
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.back :
+                startActivity(new Intent(this, LoginOrSignupActivity.class));
+                break;
+        }
     }
 
 }
