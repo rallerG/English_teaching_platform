3c95295e04ef610fb84976c2dbda822a7a2ad239
MagnusKochDTU <s175189@student.dtu.dk>
2018-11-01 13:08:42 +0100

    Change names on some components and added a scrollview for signup

--- app/src/main/java/com/gruppe/englishteachingplatfrom/signupActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/signupActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class signupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
