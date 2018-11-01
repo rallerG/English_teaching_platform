c942391fbbdcc97e661c23c61670b8bad5b92eea
MagnusKochDTU <s175189@student.dtu.dk>
2018-11-01 11:14:52 +0100

    Logic for buttons created and changed names for some classes

--- app/src/main/java/com/gruppe/englishteachingplatfrom/loginOrSignupActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/loginOrSignupActivity.java
@@ -1,13 +1,15 @@
 package com.gruppe.englishteachingplatfrom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class loginOrSignupActivity extends AppCompatActivity {
+public class LoginOrSignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,35 @@ public class loginOrSignupActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Setting ClickListeninger on the buttons
+        Button loginFacebook = (Button) findViewById(R.id.loginFacebook);
+        loginFacebook.setOnClickListener(this);
+        Button loginWeChat = (Button) findViewById(R.id.loginWeChat);
+        loginWeChat.setOnClickListener(this);
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(this);
+        Button signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.loginFacebook :
+                //startActivity(new Intent(this, FacebookActivity.class));
+                System.out.println("not done yet");
+                break;
+            case R.id.loginWeChat :
+                //startActivity(new Intent(this,WeChatActivity.class));
+                System.out.println("not done yet");
+                break;
+            case R.id.login :
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.signup :
+                startActivity(new Intent(this, SignupActivity.class));
+                break;
+        }
+    }
 }
