8b106935d414e560a2fd882c00f9a6873de1a66c
MagnusKochDTU <s175189@student.dtu.dk>
2018-11-01 14:27:29 +0100

    Implemented checking for test usernames and wrong username/password handling

--- app/src/main/java/com/gruppe/englishteachingplatfrom/loginActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/loginActivity.java
@@ -1,15 +1,22 @@
 package com.gruppe.englishteachingplatfrom;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         });
 
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
         Button signup = (Button) findViewById(R.id.login);
         signup.setOnClickListener(this);
     }
@@ -35,10 +45,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.login :
-                if (true == true)
-                    //startActivity(new Intent(this, mainActivity.class));
+                if (username.getText().equals("teacher")) {
+                    //startActivity(new Intent(this, teacherActivity.class));
                     System.out.println("not done yet");
+                }
+                else if (username.getText().equals("student")) {
+                    //startActivity(new Intent(this, studentActivity.class));
+                    System.out.println("not done yet");
+                }
+                else {
+                    wrongUseranmePasswordError();
+                }
                 break;
         }
+    }
+
+    private void wrongUseranmePasswordError() {
+        username.setError("Forkert username, for testing skriv teacher eller student");
     }
 }
