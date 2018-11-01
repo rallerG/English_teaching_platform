793f360506802124c57e792c9cef82598cdd8061
MagnusKochDTU <s175189@student.dtu.dk>
2018-11-01 14:01:08 +0100

    Implemented button functions in Login and WhatAreYouActivity

--- app/src/main/java/com/gruppe/englishteachingplatfrom/WhatAreYouActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/WhatAreYouActivity.java
@@ -1,13 +1,15 @@
 package com.gruppe.englishteachingplatfrom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class WhatAreYouActivity extends AppCompatActivity {
+public class WhatAreYouActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,28 @@ public class WhatAreYouActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button teacher = (Button) findViewById(R.id.teacher);
+        teacher.setOnClickListener(this);
+        Button student = (Button) findViewById(R.id.student);
+        student.setOnClickListener(this);
+        Button institute = (Button) findViewById(R.id.institute);
+        institute.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.teacher :
+                startActivity(new Intent(this, SignupActivity.class));
+                break;
+            case R.id.student :
+                startActivity(new Intent(this, SignupActivity.class));
+                break;
+            case R.id.institute :
+                //startActivity(new Intent(this, LoginActivity.class));
+                System.out.println("not done yet");
+                break;
+        }
+    }
 }
--- app/src/main/java/com/gruppe/englishteachingplatfrom/loginActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/loginActivity.java
@@ -1,13 +1,15 @@
 package com.gruppe.englishteachingplatfrom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,19 @@ public class LoginActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button signup = (Button) findViewById(R.id.login);
+        signup.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login :
+                if (true == true)
+                    //startActivity(new Intent(this, mainActivity.class));
+                    System.out.println("not done yet");
+                break;
+        }
+    }
 }
