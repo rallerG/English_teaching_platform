50a1a894c66bfa952c5e91fb3b6bd043265b53f7
MagnusKochDTU <s175189@student.dtu.dk>
2018-11-01 09:51:22 +0100

    Names changed and design created for two activities

--- app/src/main/java/com/gruppe/englishteachingplatfrom/loginOrSignupActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/loginOrSignupActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class loginOrSignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_loginOrSignup);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
