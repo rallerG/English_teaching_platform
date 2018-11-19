134efeec3685e4c654be08f463fd1ad3ce00fa0e
rallerG <raller799@live.dk>
2018-11-19 09:44:38 +0100

    Revert "Merge conflict resolved???"
    This reverts commit ddda0879181ca8f834630c66ac052056e24f61e6.

--- app/src/androidTest/java/com/gruppe/englishteachingplatfrom/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/gruppe/englishteachingplatfrom/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.a39.gruppe.enlglishteachingplatform;
+package com.gruppe.englishteachingplatfrom;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.a39.gruppe.enlglishteachingplatform", appContext.getPackageName());
+        assertEquals("com.gruppe.englishteachingplatfrom", appContext.getPackageName());
     }
 }
