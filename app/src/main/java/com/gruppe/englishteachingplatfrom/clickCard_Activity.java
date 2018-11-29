7c94f40aaf46210753684911afdf49de19ad6b2a
Unknown <s144146@student.dtu.dk>
2018-11-29 15:28:10 +0100

    activity click card
    activity for the card click

---
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/clickCard_Activity.java
@@ -0,0 +1,13 @@
+package com.gruppe.englishteachingplatfrom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clickCard_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_card);
+    }
+}
