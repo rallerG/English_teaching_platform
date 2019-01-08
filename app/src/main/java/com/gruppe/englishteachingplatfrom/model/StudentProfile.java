86a2ce55d3754d107268bbf66624140aa632ed86
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2019-01-08 10:42:33 +0100

    Added atributes to student class

--- app/src/main/java/com/gruppe/englishteachingplatfrom/model/StudentProfile.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/model/StudentProfile.java
@@ -1,4 +1,26 @@
 package com.gruppe.englishteachingplatfrom.model;
 
 public class StudentProfile {
+    private String id;
+    private String Name;
+    private String email;
+    private int profilePicture;
+    private double rating;
+
+    //Constructer with ID
+    public StudentProfile(String id, String name, String email, int profilePicture, double rating) {
+        this.id = id;
+        Name = name;
+        this.email = email;
+        this.profilePicture = profilePicture;
+        this.rating = rating;
+    }
+
+    //Constructer without ID for database
+    public StudentProfile(String name, String email, int profilePicture, double rating) {
+        Name = name;
+        this.email = email;
+        this.profilePicture = profilePicture;
+        this.rating = rating;
+    }
 }
