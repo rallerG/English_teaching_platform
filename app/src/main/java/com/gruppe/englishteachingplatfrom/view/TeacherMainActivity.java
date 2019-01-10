8212539306b16127f3c7a233f95db17b93b9b7fe
Thomas Mattsson <tmattsson0@gmail.com>
2019-01-10 11:43:29 +0100

    Deleted unused method

--- app/src/main/java/com/gruppe/englishteachingplatfrom/view/TeacherMainActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/view/TeacherMainActivity.java
@@ -53,40 +53,7 @@ public class TeacherMainActivity extends AppCompatActivity
         displaySelectedScreen(R.id.nav_settings);
 
     }
-
-    private void getImages(){
-      //  Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
-
-        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        mNames.add("Havasu Falls");
-
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
-
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
-
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
-
-
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Mahahual");
-
-        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-        mNames.add("Frozen Lake");
-
-
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("White Sands Desert");
-
-        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-        mNames.add("Austrailia");
-
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Washington");
-
-    }
+    
 
     @Override
     public void onBackPressed() {
