a32ac645e3d193b74f27fc336c53b651f12cbe1c
Thomas Mattsson <tmattsson0@gmail.com>
2019-01-15 11:12:20 +0100

    Revert "Revert "Added progress dialog box""
    This reverts commit 28f62e87c3b9c6c0f5d3d5325af30c20f2481c24.

--- app/src/main/java/com/gruppe/englishteachingplatfrom/view/LoginOrSignupActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/view/LoginOrSignupActivity.java
@@ -1,5 +1,6 @@
 package com.gruppe.englishteachingplatfrom.view;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -26,10 +27,12 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginOrSignupActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ProgressDialog pDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Singleton.downloadAllTeachersProfiles();
-        Singleton.downloadAllStudentProfiles();
+//        Singleton.downloadAllTeachersProfiles();
+//        Singleton.downloadAllStudentProfiles();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_login_or_signup);
 
@@ -47,13 +50,24 @@ public class LoginOrSignupActivity extends AppCompatActivity implements View.OnC
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.loginFacebook :
+                // Showing progress dialog to user
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Please wait...");
+                pDialog.setCancelable(true);
+                pDialog.show();
+
                 final Intent intent1 = new Intent(this, MainActivity.class);
                 TeachersDocument teachersDocument = new TeachersDocumentImpl();
+                Singleton.downloadAllTeachersProfiles();
                 teachersDocument.getAll(new CallbackList<TeacherProfile>() {
                     @Override
                     public void onCallback(List<TeacherProfile> listOfObjects) {
-                        Singleton.downloadAllTeachersProfiles();
+//                        Singleton.downloadAllTeachersProfiles();
+
                         //loading bar
+                        if (pDialog.isShowing()){
+                            pDialog.dismiss();
+                        }
                         startActivity(intent1);
                         finish();
                         System.out.println("Testing");
@@ -61,21 +75,30 @@ public class LoginOrSignupActivity extends AppCompatActivity implements View.OnC
                 });
                 break;
             case R.id.loginWeChat :
+                // Showing progress dialog to user
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Please wait...");
+                pDialog.setCancelable(true);
+                pDialog.show();
+
                 final Intent intent2 = new Intent(this, TeacherMainActivity.class);
                 StudentsDocument studentsDocument = new StudentsDocumentImpl();
+                Singleton.downloadAllStudentProfiles();
                 studentsDocument.getAll(new CallbackList<StudentProfile>() {
                     @Override
                     public void onCallback(List<StudentProfile> listOfObjects) {
-                        Singleton.downloadAllStudentProfiles();
                         //loading bar
+                        if (pDialog.isShowing()){
+                            pDialog.dismiss();
+                        }
                         startActivity(intent2);
                         finish();
                         System.out.println("Testing");
                     }
                 });
-                startActivity(new Intent(this, TeacherMainActivity.class));
-                finish();
-                System.out.println("Testing");
+//                startActivity(new Intent(this, TeacherMainActivity.class));
+//                finish();
+//                System.out.println("Testing");
                 break;
             case R.id.login :
                 startActivity(new Intent(this, LoginActivity.class));
