576a80f56ab9ad2b845325b4a365382236dd1f18
rallerG <raller799@live.dk>
2019-01-15 13:30:05 +0100

    Remove old test code

--- app/src/main/java/com/gruppe/englishteachingplatfrom/view/LoginOrSignupActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/view/LoginOrSignupActivity.java
@@ -9,10 +9,14 @@ import android.widget.Button;
 
 import com.gruppe.englishteachingplatfrom.R;
 import com.gruppe.englishteachingplatfrom.backend.implementations.StudentsDocumentImpl;
+import com.gruppe.englishteachingplatfrom.backend.implementations.TeacherFeedbackDocumentImpl;
 import com.gruppe.englishteachingplatfrom.backend.implementations.TeachersDocumentImpl;
+import com.gruppe.englishteachingplatfrom.backend.interfaces.Callback;
 import com.gruppe.englishteachingplatfrom.backend.interfaces.CallbackList;
 import com.gruppe.englishteachingplatfrom.backend.interfaces.StudentsDocument;
+import com.gruppe.englishteachingplatfrom.backend.interfaces.TeacherFeedbackDocument;
 import com.gruppe.englishteachingplatfrom.backend.interfaces.TeachersDocument;
+import com.gruppe.englishteachingplatfrom.model.Feedback;
 import com.gruppe.englishteachingplatfrom.model.Singleton;
 import com.gruppe.englishteachingplatfrom.model.StudentProfile;
 import com.gruppe.englishteachingplatfrom.model.TeacherProfile;
@@ -39,13 +43,6 @@ public class LoginOrSignupActivity extends AppCompatActivity implements View.OnC
         login.setOnClickListener(this);
         Button signup =  findViewById(R.id.signup);
         signup.setOnClickListener(this);
-
-        TeacherFeedbackDocument teacherFeedbackDocument = new TeacherFeedbackDocumentImpl("1");
-        teacherFeedbackDocument.get("1", new Callback<FeedbackProfile>() {
-            @Override
-            public void onCallback(FeedbackProfile object) {
-            }
-        });
     }
     @Override
     public void onClick(View view) {
