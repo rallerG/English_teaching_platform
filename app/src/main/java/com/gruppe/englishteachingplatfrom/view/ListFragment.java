68c205d2d9188eb534623082f7d030b5fc098969
rallerG <raller799@live.dk>
2019-01-17 11:52:32 +0100

    Student list works with backend and is done for now

--- app/src/main/java/com/gruppe/englishteachingplatfrom/view/ListFragment.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/view/ListFragment.java
@@ -91,31 +91,32 @@ public class ListFragment extends Fragment {
                                 @Override
                                 public void onCallback(TeacherProfile object) {
                                     p.getCurrrentStudent().getMatchProfiles().add(object);
+                                    System.out.println(p.getCurrrentStudent().getMatchProfiles());
+                                    mAdapter = new MyFavoriteRecyclerViewAdapter(p.getCurrrentStudent().getMatchProfiles());
+                                    mRecyclerView.setHasFixedSize(true);
+                                    mRecyclerView.setLayoutManager(mLayoutManager);
+                                    mRecyclerView.setAdapter(mAdapter);
+                                    mAdapter.setOnItemClickListener(new MyFavoriteRecyclerViewAdapter.OnItemClickListener() {
+
+                                        @Override
+                                        public void onItemClick(int position) {
+                                            if(!clicked) {
+                                                //What to do in click
+                                                Intent i = new Intent(getActivity(), TeacherInfoActivity.class);
+                                                i.putExtra("name", p.getCurrrentStudent().getMatchProfiles().get(position).getName());
+                                                i.putExtra("price", p.getCurrrentStudent().getMatchProfiles().get(position).getPrice());
+                                                i.putExtra("rate", p.getCurrrentStudent().getMatchProfiles().get(position).getRating());
+                                                i.putExtra("language", p.getCurrrentStudent().getMatchProfiles().get(position).getLanguage());
+                                                //remember information and description text (when objects are used)
+                                                startActivity(i);
+                                                clicked = true;
+                                            }
+                                        }
+                                    });
                                 }
                             });
                         }
 //                        p.getCurrrentStudent().getMatchProfiles().addAll(listOfObjects);
-                        mAdapter = new MyFavoriteRecyclerViewAdapter(p.getCurrrentStudent().getMatchProfiles());
-                        mRecyclerView.setHasFixedSize(true);
-                        mRecyclerView.setLayoutManager(mLayoutManager);
-                        mRecyclerView.setAdapter(mAdapter);
-                        mAdapter.setOnItemClickListener(new MyFavoriteRecyclerViewAdapter.OnItemClickListener() {
-
-                            @Override
-                            public void onItemClick(int position) {
-                                if(!clicked) {
-                                    //What to do in click
-                                    Intent i = new Intent(getActivity(), TeacherInfoActivity.class);
-                                    i.putExtra("name", p.getCurrrentStudent().getMatchProfiles().get(position).getName());
-                                    i.putExtra("price", p.getCurrrentStudent().getMatchProfiles().get(position).getPrice());
-                                    i.putExtra("rate", p.getCurrrentStudent().getMatchProfiles().get(position).getRating());
-                                    i.putExtra("language", p.getCurrrentStudent().getMatchProfiles().get(position).getLanguage());
-                                    //remember information and description text (when objects are used)
-                                    startActivity(i);
-                                    clicked = true;
-                                }
-                            }
-                        });
                     }
                 });
                 break;
@@ -132,38 +133,38 @@ public class ListFragment extends Fragment {
                                 @Override
                                 public void onCallback(TeacherProfile object) {
                                     p.getCurrrentStudent().getFavoriteProfiles().add(object);
+                                    mAdapter = new MyFavoriteRecyclerViewAdapter(p.getCurrrentStudent().getFavoriteProfiles());
+                                    mRecyclerView.setHasFixedSize(true);
+                                    mRecyclerView.setLayoutManager(mLayoutManager);
+                                    mRecyclerView.setAdapter(mAdapter);
+                                    mAdapter.setOnItemClickListener(new MyFavoriteRecyclerViewAdapter.OnItemClickListener() {
+
+
+                                        @Override
+                                        public void onItemClick(int position) {
+                                            if(!clicked) {
+                                                //What to do in click
+                                                System.out.println("ListFragment.java: Du har trykket på : " + p.getCurrrentStudent().getFavoriteProfiles().get(position).getName());
+                                                Intent i = new Intent(getActivity(), TeacherInfoActivity.class);
+            //                                    i.putExtra("name", p.getTeacherDummies().get(position).getName());
+            //                                    i.putExtra("price", p.getTeacherDummies().get(position).getPrice());
+            //                                    i.putExtra("rate", p.getTeacherDummies().get(position).getRating());
+            //                                    i.putExtra("language", p.getTeacherDummies().get(position).getLanguage());
+                                                i.putExtra("name", p.getCurrrentStudent().getFavoriteProfiles().get(position).getName());
+                                                i.putExtra("price", p.getCurrrentStudent().getFavoriteProfiles().get(position).getPrice());
+                                                i.putExtra("rate", p.getCurrrentStudent().getFavoriteProfiles().get(position).getRating());
+                                                i.putExtra("language", p.getCurrrentStudent().getFavoriteProfiles().get(position).getLanguage());
+                                                //remember information and description text (when objects are used)
+                                                startActivity(i);
+                                                clicked = true;
+                                            }
+                                        }
+                                    });
                                 }
                             });
                         }
 
 //                        p.getCurrrentStudent().getFavoriteProfiles().addAll(listOfObjects);
-                        mAdapter = new MyFavoriteRecyclerViewAdapter(p.getCurrrentStudent().getFavoriteProfiles());
-                        mRecyclerView.setHasFixedSize(true);
-                        mRecyclerView.setLayoutManager(mLayoutManager);
-                        mRecyclerView.setAdapter(mAdapter);
-                        mAdapter.setOnItemClickListener(new MyFavoriteRecyclerViewAdapter.OnItemClickListener() {
-
-
-                            @Override
-                            public void onItemClick(int position) {
-                                if(!clicked) {
-                                    //What to do in click
-                                    System.out.println("ListFragment.java: Du har trykket på : " + p.getCurrrentStudent().getFavoriteProfiles().get(position).getName());
-                                    Intent i = new Intent(getActivity(), TeacherInfoActivity.class);
-//                                    i.putExtra("name", p.getTeacherDummies().get(position).getName());
-//                                    i.putExtra("price", p.getTeacherDummies().get(position).getPrice());
-//                                    i.putExtra("rate", p.getTeacherDummies().get(position).getRating());
-//                                    i.putExtra("language", p.getTeacherDummies().get(position).getLanguage());
-                                    i.putExtra("name", p.getCurrrentStudent().getFavoriteProfiles().get(position).getName());
-                                    i.putExtra("price", p.getCurrrentStudent().getFavoriteProfiles().get(position).getPrice());
-                                    i.putExtra("rate", p.getCurrrentStudent().getFavoriteProfiles().get(position).getRating());
-                                    i.putExtra("language", p.getCurrrentStudent().getFavoriteProfiles().get(position).getLanguage());
-                                    //remember information and description text (when objects are used)
-                                    startActivity(i);
-                                    clicked = true;
-                                }
-                            }
-                        });
                     }
                 });
                 break;
@@ -179,39 +180,39 @@ public class ListFragment extends Fragment {
                                 @Override
                                 public void onCallback(TeacherProfile object) {
                                     p.getCurrrentStudent().getPendingProfiles().add(object);
+                                    mAdapter = new MyFavoriteRecyclerViewAdapter(p.getCurrrentStudent().getPendingProfiles());
+                                    mRecyclerView.setHasFixedSize(true);
+                                    mRecyclerView.setLayoutManager(mLayoutManager);
+                                    mRecyclerView.setAdapter(mAdapter);
+                                    mAdapter.setOnItemClickListener(new MyFavoriteRecyclerViewAdapter.OnItemClickListener() {
+
+
+                                        @Override
+                                        public void onItemClick(int position) {
+                                            if(!clicked) {
+                                                //What to do in click
+                                                System.out.println("ListFragment.java: Du har trykket på : " + p.getCurrrentStudent().getPendingProfiles().get(position).getName());
+                                                Intent i = new Intent(getActivity(), TeacherInfoActivity.class);
+            //                                    i.putExtra("name", p.getTeacherDummies().get(position).getName());
+            //                                    i.putExtra("price", p.getTeacherDummies().get(position).getPrice());
+            //                                    i.putExtra("rate", p.getTeacherDummies().get(position).getRating());
+            //                                    i.putExtra("language", p.getTeacherDummies().get(position).getLanguage());
+                                                i.putExtra("name", p.getCurrrentStudent().getPendingProfiles().get(position).getName());
+                                                i.putExtra("price", p.getCurrrentStudent().getPendingProfiles().get(position).getPrice());
+                                                i.putExtra("rate", p.getCurrrentStudent().getPendingProfiles().get(position).getRating());
+                                                i.putExtra("language", p.getCurrrentStudent().getPendingProfiles().get(position).getLanguage());
+                                                //remember information and description text (when objects are used)
+                                                startActivity(i);
+                                                clicked = true;
+                                            }
+                                        }
+                                    });
                                 }
                             });
                         }
 
 
 //                        p.getCurrrentStudent().getPendingProfiles().addAll(listOfObjects);
-                        mAdapter = new MyFavoriteRecyclerViewAdapter(p.getCurrrentStudent().getPendingProfiles());
-                        mRecyclerView.setHasFixedSize(true);
-                        mRecyclerView.setLayoutManager(mLayoutManager);
-                        mRecyclerView.setAdapter(mAdapter);
-                        mAdapter.setOnItemClickListener(new MyFavoriteRecyclerViewAdapter.OnItemClickListener() {
-
-
-                            @Override
-                            public void onItemClick(int position) {
-                                if(!clicked) {
-                                    //What to do in click
-                                    System.out.println("ListFragment.java: Du har trykket på : " + p.getCurrrentStudent().getPendingProfiles().get(position).getName());
-                                    Intent i = new Intent(getActivity(), TeacherInfoActivity.class);
-//                                    i.putExtra("name", p.getTeacherDummies().get(position).getName());
-//                                    i.putExtra("price", p.getTeacherDummies().get(position).getPrice());
-//                                    i.putExtra("rate", p.getTeacherDummies().get(position).getRating());
-//                                    i.putExtra("language", p.getTeacherDummies().get(position).getLanguage());
-                                    i.putExtra("name", p.getCurrrentStudent().getPendingProfiles().get(position).getName());
-                                    i.putExtra("price", p.getCurrrentStudent().getPendingProfiles().get(position).getPrice());
-                                    i.putExtra("rate", p.getCurrrentStudent().getPendingProfiles().get(position).getRating());
-                                    i.putExtra("language", p.getCurrrentStudent().getPendingProfiles().get(position).getLanguage());
-                                    //remember information and description text (when objects are used)
-                                    startActivity(i);
-                                    clicked = true;
-                                }
-                            }
-                        });
                     }
                 });
                 break;
