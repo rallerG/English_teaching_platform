d50b8e4fe67559992ceabf76690cf359a3040eec
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-01-21 17:20:45 +0100

    made get fragment in adapter

--- app/src/main/java/com/gruppe/englishteachingplatfrom/controller/FragPagerAdapter.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/controller/FragPagerAdapter.java
@@ -3,15 +3,21 @@ package com.gruppe.englishteachingplatfrom.controller;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.SparseArray;
 import android.view.View;
 
 
 import com.gruppe.englishteachingplatfrom.model.Singleton;
 import com.gruppe.englishteachingplatfrom.view.PageFragment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FragPagerAdapter extends FragmentStatePagerAdapter implements View.OnClickListener {
 
     private Singleton p = Singleton.getInstance();
+    private Map<Integer, Fragment> fragmentMap = new HashMap<>();
+    private SparseArray<Fragment> fragmentSparseArray = new SparseArray<Fragment>();
 //    private ArrayList<TeacherProfile> contents = p.getTeacherDummies();
 
 
@@ -21,8 +27,9 @@ public class FragPagerAdapter extends FragmentStatePagerAdapter implements View.
 
     @Override
     public Fragment getItem(int position) {
-
-        return PageFragment.newInstance(position);
+        Fragment page = PageFragment.newInstance(position);
+        fragmentSparseArray.append(position,page);
+        return page;
     }
 
 //    @Override
@@ -42,6 +49,10 @@ public class FragPagerAdapter extends FragmentStatePagerAdapter implements View.
         return p.getTeacherDummies().size();
     }
 
+
+    public Fragment getFragment(int position){
+        return fragmentSparseArray.get(position);
+    }
 
     @Override
     public void onClick(View v) {
