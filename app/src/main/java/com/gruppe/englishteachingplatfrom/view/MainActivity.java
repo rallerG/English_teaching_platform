54b253e2973a9543f5063f0f9b9237e7a1a03b61
Thomas Mattsson <tmattsson0@gmail.com>
2019-01-24 15:02:42 +0100

    Fixed bug and change name

--- app/src/main/java/com/gruppe/englishteachingplatfrom/view/MainActivity.java
+++ app/src/main/java/com/gruppe/englishteachingplatfrom/view/MainActivity.java
@@ -231,7 +231,7 @@ public class MainActivity extends AppCompatActivity
                 ListFragment.clicked = false;
                 fragment = new FragPager();
                 fragment.setArguments(args);
-                setTitle("Gruppe Magnus");
+                setTitle("TeachENG");
                 break;
             case R.id.nav_matches:
                 PageFragment.clicked = false;
